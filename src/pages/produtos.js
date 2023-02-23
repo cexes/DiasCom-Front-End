4624771bdea2f2eb5e1b33c9d0f35988b2947f2e
Cesar <mottacesar001@gmail.com>
2023-02-23 12:23:31 -0500

    PROXY is working, consuming info through back-end

--- src/pages/produtos.js
+++ src/pages/produtos.js
@@ -1,15 +1,42 @@
-import styles from '../components/prd.module.css'
+import React,{useEffect,useState} from 'react'; 
+import styles from '../components/prd.module.css';
 
 
 function PrPage() {
-    return(
-        <div>
-            <div className= {styles.container}>
+ 
+  const [backEndData, setBackEndData] = useState([{}])
+   useEffect(()=>{
+     fetch("/produtos").then(
+       response => response.json()
+    ).then(
+        data =>{
+          setBackEndData(data);
+        }
+      )
+  },[])
+
+  return(
+        
+        <div className= {styles.container}>
             <h1>Produtos</h1>
-            </div>
+
+             <div>
+               {(typeof backEndData.singers === 'undefined')?(
+                    <p>Loading </p>
+                ):(
+                  backEndData.singers.map((singers,i)=>(
+                  <p key={i}>{singers} <br></br></p>
+                      
+              ))
+            )}                     
+
+ 
+
+             </div>
+        </div>
+
            
 
-        </div>
     )
 }
 
