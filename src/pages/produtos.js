cbd2ab2b69cd70c611570da578f0ee11bae5ee5f
Cesar <mottacesar001@gmail.com>
2023-03-05 14:26:26 -0500

    consuming api

--- src/pages/produtos.js
+++ src/pages/produtos.js
@@ -1,41 +1,37 @@
-import React,{useEffect,useState} from 'react'; 
+import axios from 'axios';
+import React, {useEffect,useState } from 'react'; 
 import styles from '../components/prd.module.css';
 
-
 function PrPage() {
- 
-  const [backEndData, setBackEndData] = useState([{}])
-   useEffect(()=>{
-     fetch("/produtos").then(
-       response => response.json()
-    ).then(
-        data =>{
-          setBackEndData(data);
-        }
-      )
-  },[])
 
-  return(
-        
-        <div className= {styles.container}>
+   const [prds, setPrds] = useState([]);
+   const getPrds = async () => {
+        const response =  await axios.get("/produtos");
+        const data = response.data
+        console.log(data)
+        setPrds(data);
+   };
+
+    useEffect(()=>{
+       getPrds(); 
+    },[]);
+
+
+    return(    
+        <div className = {styles.container}>
             <h1>Produtos</h1>
+            {prds.length === 0 ? (<p>Caregando...</p>) : (
+                prds.map((produtos) =>(
+                    <div key={produtos.id}> 
+                      <h2>{produtos.type}</h2>
+                     
 
-             <div>
-               {(typeof backEndData.singers === 'undefined')?(
-                    <p>Loading </p>
-                ):(
-                  backEndData.singers.map((singers,i)=>(
-                  <p key={i}>{singers} <br></br></p>
-                      
-              ))
-            )}                     
-
- 
-
-             </div>
+                    </div>
+                ))
+            )}
+    
+            
         </div>
-
-           
 
     )
 }
