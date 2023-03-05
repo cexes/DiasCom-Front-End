864f11b63a3000245cb21b6a8802faa34edca7f9
Cesar <mottacesar001@gmail.com>
2023-03-05 16:55:24 -0500

    style page prd

--- src/pages/produtos.js
+++ src/pages/produtos.js
@@ -19,13 +19,18 @@ function PrPage() {
 
     return(    
         <div className = {styles.container}>
-            <h1>Produtos</h1>
+            <h1>Produtos:</h1>
             {prds.length === 0 ? (<p>Caregando...</p>) : (
                 prds.map((produtos) =>(
-                    <div key={produtos.id}> 
-                      <h2>{produtos.type}</h2>
-                     
+                    <div className= {styles.boxPrd} key={produtos.id}> 
+                       <div> 
+                         <img src='r'/>
+                          <p>{produtos.type}</p>
+                          <p>{produtos.price}</p>
+                          <p>{produtos.description}</p>
+                        
 
+                        </div>
                     </div>
                 ))
             )}
